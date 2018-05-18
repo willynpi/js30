608a73e6309caa48fd6db3d0838a0ac4ab3b0a1c
Ying <afji2039@gmail.com>
2018-05-18 23:33:12 +0800

    let -> const

--- css-js-clock/css-js-clock.js
+++ css-js-clock/css-js-clock.js
@@ -6,19 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const leftTimer = () => {
         const date = new Date();
 
-        let second = date.getSeconds();
-        let secondDegree = (second * 6) + 90;
+        const second = date.getSeconds();
+        const secondDegree = (second * 6) + 90;
         // 360 degree for 60 seconds, 6 degree per second.
         secondHand.style.transform =  `rotate(${secondDegree}deg)`;
 
-        let minute = date.getMinutes();
-        let minuteDegree = (minute * 6) + 90 + (second / 10);
+        const minute = date.getMinutes();
+        const minuteDegree = (minute * 6) + 90 + (second / 10);
         // 360 degree for 60 seconds, 6 degree per minute. 
         // plus 6 degree for 60 seconds, 1 degree every 10 seconds.        
         minuteHand.style.transform =  `rotate(${minuteDegree}deg)`;
         
-        let hour = date.getHours();
-        let hourDegree = (hour * 30) + 90 + (minute / 2);
+        const hour = date.getHours();
+        const hourDegree = (hour * 30) + 90 + (minute / 2);
         // 360 degree for 12 hours, 30 degree per hour. 
         // plus 30 degree for 60 minute, 1 degree every 2 minutes.
         hourHand.style.transform =  `rotate(${hourDegree}deg)`;
@@ -26,9 +26,9 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(leftTimer, 1000);
     
     ////// Right Clock //////
-    let secondHandLeft = document.querySelector('.right-clock .second-hand');
-    let minuteHandLeft = document.querySelector('.right-clock .minute-hand'); 
-    let hourHandLeft = document.querySelector('.right-clock .hour-hand'); 
+    const secondHandLeft = document.querySelector('.right-clock .second-hand');
+    const minuteHandLeft = document.querySelector('.right-clock .minute-hand'); 
+    const hourHandLeft = document.querySelector('.right-clock .hour-hand'); 
     const rightTimer = () => {
         const now = new Date();
 
