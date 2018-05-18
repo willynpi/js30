b8387068a317ca24064b542cd1c7117696ca0e4f
Ying <afji2039@gmail.com>
2018-05-18 12:29:38 +0800

    approached by getting real hour,minute,second

--- css-js-clock/css-js-clock.js
+++ css-js-clock/css-js-clock.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const timer = () => {
-        
+    const rightTimer = () => {
+        let date = new Date();
+        let secondHand = document.querySelector('.second');
+        let hourHand = document.querySelector('.hour');
+        let minuteHand = document.querySelector('.minute');
+        let minute = date.getMinutes();
+        let hour = date.getHours();
+        let second = date.getSeconds();
+        secondHand.style.transform =  `rotate(${second*6+90}deg)`;
+        minuteHand.style.transform =  `rotate(${minute*6+90}deg)`;
+        hourHand.style.transform =  `rotate(${hour*30+90+minute/2}deg)`;
     }
+    setInterval(rightTimer, 1000);
 });
