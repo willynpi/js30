2aae83f0eedd69aac434aaad027c03794a71a8e0
Ying <afji2039@gmail.com>
2018-06-16 10:33:27 +0800

    done with js30 official

--- flex-panels-image-gallery/flex-panels-image-gallery.js
+++ flex-panels-image-gallery/flex-panels-image-gallery.js
@@ -2,14 +2,16 @@ const panels = document.querySelectorAll('.panel');
 
 function toggleOpen() {
     this.classList.toggle('open');
-}
-function toggleActive(e) {
-    console.log(e.propertyName);
     this.classList.toggle('open-active');
 }
-// var toggleOpen = (e) => {
-//     console.log(this);
-//     e.target.classList.toggle('open');
-// }
+
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+/* js30 official , but I prefer to fire the events at the same time. */
+// function toggleActive(e) {
+//     if (e.propertyName.includes("flex")) {
+//         // 'flex-grow' in Chrome and others, while 'flex' only in Safari
+//         this.classList.toggle('open-active');
+//     }
+// }
+// panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
