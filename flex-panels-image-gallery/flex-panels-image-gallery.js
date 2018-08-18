dc741c5d66d950cea61e4502085528c164c2fc7e
Ying <afji2039@gmail.com>
2018-08-18 23:29:41 +0800

    compress images, link open in new page

--- flex-panels-image-gallery/flex-panels-image-gallery.js
+++ flex-panels-image-gallery/flex-panels-image-gallery.js
@@ -1,16 +1,17 @@
 const panels = document.querySelectorAll('.panel');
-
+const links = document.querySelectorAll('.panel p a');
 function toggleOpen() {
     this.classList.toggle('open');
     this.classList.toggle('open-active');
 }
-
+/* prevent panels close when clicking on the links */
+links.forEach(link => link.addEventListener('click', e => e.stopPropagation())); 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 
 /* js30 official , but I prefer to fire the events at the same time. */
 // function toggleActive(e) {
 //     if (e.propertyName.includes("flex")) {
-//         // 'flex-grow' in Chrome and others, while 'flex' only in Safari
+//         //'flex-grow' in Chrome and others, while 'flex' only in Safari
 //         this.classList.toggle('open-active');
 //     }
 // }
