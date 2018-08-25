3da6de6ee3877973f614894f9c85fb91d9e5f75d
Ying <afji2039@gmail.com>
2018-08-25 17:07:36 +0800

    split into to part, mine and official's

--- hold-shift-to-check-mulitple-checkboxes/hscmc.js
+++ hold-shift-to-check-mulitple-checkboxes/hscmc.js
@@ -1,20 +1,54 @@
 const lis = document.querySelectorAll('li');
 let startClick = 0;
 let currentClick = 0;
-lis.forEach(el=>{
+lis.forEach(el => {
     el.addEventListener('click', (e) => {
         if(e.shiftKey) {
             currentClick = el.dataset.id;
-            for(let i = parseInt(startClick) + 1; i <= currentClick; i++) {
-                tElement = document.querySelector(`li[data-id="${i}"]`);
-                tElement.classList.toggle('checked');
-                tElement.children[0].children[0].checked = !tElement.children[0].children[0].checked;
+            if(currentClick > startClick) {
+                for(let i = parseInt(startClick) + 1; i <= currentClick; i++) {
+                    tElement = document.querySelector(`li[data-id="${i}"]`);
+                    tElement.classList.toggle('checked');
+                    tElement.children[0].children[0].checked = !tElement.children[0].children[0].checked;
+                }
+            } else {
+                for(let i = parseInt(currentClick); i < startClick; i++) {
+                    tElement = document.querySelector(`li[data-id="${i}"]`);
+                    tElement.classList.toggle('checked');
+                    tElement.children[0].children[0].checked = !tElement.children[0].children[0].checked;
+                }
             }
+            
         } else {
             el.classList.toggle('checked');
             el.children[0].children[0].checked = !el.children[0].children[0].checked;
         }
         startClick = el.dataset.id;
     });
+});
+
+const items = document.querySelectorAll('.items');
+const divsCheckboxed = document.querySelectorAll('.official input[type=checkbox]');
+let lastChecked = 0
+let currentChecked = 0
+const handleCheck = (e) => {
+    currentChecked = e.target.previousElementSibling.children[0];
+    currentChecked.checked = true;
+    let inBetween = false;
+    checkboxed.forEach(box => {
+        if(e.shiftKey && startClick) {
+            if(box === startClick || box === currentChecked ) {
+                inBetween = !inBetween;
+            }
+            if(inBetween) {
+                box.checked = true;
+            }
+        }
+    });
+    lastChecked = currentChecked;
+}
+
+items.forEach(el => {
+    el.addEventListener('click', handleCheck);   
 });
 
