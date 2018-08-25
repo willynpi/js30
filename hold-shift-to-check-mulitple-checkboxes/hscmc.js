22f30e3be65745f045e6f726224d61c77bdc0c75
Ying <afji2039@gmail.com>
2018-08-25 15:36:06 +0800

    my solution

--- hold-shift-to-check-mulitple-checkboxes/hscmc.js
+++ hold-shift-to-check-mulitple-checkboxes/hscmc.js
@@ -1,7 +1,20 @@
 const lis = document.querySelectorAll('li');
+let startClick = 0;
+let currentClick = 0;
 lis.forEach(el=>{
-    el.addEventListener('click', () => {
-        el.classList.toggle('checked');
-        el.children[0].children[0].checked = !el.children[0].children[0].checked;
+    el.addEventListener('click', (e) => {
+        if(e.shiftKey) {
+            currentClick = el.dataset.id;
+            for(let i = parseInt(startClick) + 1; i <= currentClick; i++) {
+                tElement = document.querySelector(`li[data-id="${i}"]`);
+                tElement.classList.toggle('checked');
+                tElement.children[0].children[0].checked = !tElement.children[0].children[0].checked;
+            }
+        } else {
+            el.classList.toggle('checked');
+            el.children[0].children[0].checked = !el.children[0].children[0].checked;
+        }
+        startClick = el.dataset.id;
     });
 });
+
